631296d8f360b1e26899ba97255742bc545cfc8b
Jingnan_Si <Jingnan_Si@d55sn2x>
2010-03-24 16:18:05 +0800

    start server connect

---
+++ src/protocols/mule/Mule/Network/EncryptedDatagramSocket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mule.Network
+{
+    public interface EncryptedDatagramSocket
+    {
+        int DecryptReceivedClient(byte[] pbyBufIn, int nBufLen,
+            out byte[] ppbyBufOut, uint dwIP, 
+            out uint nReceiverVerifyKey, out uint nSenderVerifyKey);
+        int EncryptSendClient(out byte[] ppbyBuf, int nBufLen,
+            byte[] pachClientHashOrKadID, bool bKad,
+            uint nReceiverVerifyKey, uint nSenderVerifyKey);
+        int DecryptReceivedServer(byte[] pbyBufIn, int nBufLen,
+            out byte[] ppbyBufOut, uint dwBaseKey, uint dbgIP);
+        int EncryptSendServer(out byte[] ppbyBuf, int nBufLen, uint dwBaseKey);
+    }
+}

bd18954b034ed2106d08ef545042e5d18bf3d786
Jingnan_Si <Jingnan_Si@d55sn2x>
2010-03-24 15:43:47 +0800

    finished pinger

--- src/protocols/mule/Mule.Core/Impl/Pinger.cs
+++ src/protocols/mule/Mule.Core/Impl/Pinger.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.NetworkInformation;
+using System.Net;
 
 namespace Mule.Core.Impl
 {
@@ -9,33 +11,22 @@ namespace Mule.Core.Impl
     {
         public const uint DEFAULT_TTL = 64;
 
-        public const uint IP_STATUS_BASE = 11000;
-        public const uint IP_SUCCESS = 0;
-        public const uint IP_BUF_TOO_SMALL = (IP_STATUS_BASE + 1);
-        public const uint IP_DEST_NET_UNREACHABLE = (IP_STATUS_BASE + 2);
-        public const uint IP_DEST_HOST_UNREACHABLE = (IP_STATUS_BASE + 3);
-        public const uint IP_DEST_PROT_UNREACHABLE = (IP_STATUS_BASE + 4);
-        public const uint IP_DEST_PORT_UNREACHABLE = (IP_STATUS_BASE + 5);
-        public const uint IP_NO_RESOURCES = (IP_STATUS_BASE + 6);
-        public const uint IP_BAD_OPTION = (IP_STATUS_BASE + 7);
-        public const uint IP_HW_ERROR = (IP_STATUS_BASE + 8);
-        public const uint IP_PACKET_TOO_BIG = (IP_STATUS_BASE + 9);
-        public const uint IP_REQ_TIMED_OUT = (IP_STATUS_BASE + 10);
-        public const uint IP_BAD_REQ = (IP_STATUS_BASE + 11);
-        public const uint IP_BAD_ROUTE = (IP_STATUS_BASE + 12);
-        public const uint IP_TTL_EXPIRED_TRANSIT = (IP_STATUS_BASE + 13);
-        public const uint IP_TTL_EXPIRED_REASSEM = (IP_STATUS_BASE + 14);
-        public const uint IP_PARAM_PROBLEM = (IP_STATUS_BASE + 15);
-        public const uint IP_SOURCE_QUENCH = (IP_STATUS_BASE + 16);
-        public const uint IP_OPTION_TOO_BIG = (IP_STATUS_BASE + 17);
-        public const uint IP_BAD_DESTINATION = (IP_STATUS_BASE + 18);
-        public const uint IP_ADDR_DELETED = (IP_STATUS_BASE + 19);
-        public const uint IP_SPEC_MTU_CHANGE = (IP_STATUS_BASE + 20);
-        public const uint IP_MTU_CHANGE = (IP_STATUS_BASE + 21);
-        public const uint IP_UNLOAD = (IP_STATUS_BASE + 22);
-        public const uint IP_GENERAL_FAILURE = (IP_STATUS_BASE + 50);
-        public const uint MAX_IP_STATUS = IP_GENERAL_FAILURE;
-        public const uint IP_PENDING = (IP_STATUS_BASE + 255);
+        public const IPStatus IP_SUCCESS = (uint)IPStatus.Success;
+        public const IPStatus IP_DEST_NET_UNREACHABLE = IPStatus.DestinationNetworkUnreachable;
+        public const IPStatus IP_DEST_HOST_UNREACHABLE = IPStatus.DestinationHostUnreachable;
+        public const IPStatus IP_DEST_PROT_UNREACHABLE = IPStatus.DestinationProtocolUnreachable;
+        public const IPStatus IP_DEST_PORT_UNREACHABLE = IPStatus.DestinationPortUnreachable;
+        public const IPStatus IP_NO_RESOURCES = IPStatus.NoResources;
+        public const IPStatus IP_BAD_OPTION = IPStatus.BadOption;
+        public const IPStatus IP_HW_ERROR = IPStatus.HardwareError;
+        public const IPStatus IP_PACKET_TOO_BIG = IPStatus.PacketTooBig;
+        public const IPStatus IP_REQ_TIMED_OUT = IPStatus.TimedOut;
+        public const IPStatus IP_BAD_ROUTE = IPStatus.BadRoute;
+        public const IPStatus IP_TTL_EXPIRED_TRANSIT = IPStatus.TtlExpired;
+        public const IPStatus IP_TTL_EXPIRED_REASSEM = IPStatus.TtlReassemblyTimeExceeded;
+        public const IPStatus IP_PARAM_PROBLEM = IPStatus.ParameterProblem;
+        public const IPStatus IP_SOURCE_QUENCH = IPStatus.SourceQuench;
+        public const IPStatus IP_BAD_DESTINATION = IPStatus.BadDestination;
     }
 
     struct PingStatus
@@ -43,32 +34,64 @@ namespace Mule.Core.Impl
         public PingStatus(int dummy)
         {
             success = false;
-            status = 0;
+            status =  IPStatus.Success;
             delay = 0;
             destinationAddress = 0;
             ttl = 0;
-            error = 0;
+            error = IPStatus.Success;
         }
 
         public bool success;
-        public uint status;
+        public IPStatus status;
         public float delay;
         public uint destinationAddress;
         public uint ttl;
 
-        public uint error;
+        public IPStatus error;
     };
 
     class Pinger
     {
-        internal PingStatus Ping(uint curAddress, uint ttl, bool p, bool useUdp)
+        public PingStatus Ping(uint curAddress)
         {
-            throw new NotImplementedException();
+            return Ping(curAddress, PingerConstants.DEFAULT_TTL, false, false);
         }
 
-        internal void PIcmpErr(uint p)
+        public PingStatus Ping(uint curAddress, uint ttl)
         {
-            throw new NotImplementedException();
+            return Ping(curAddress, ttl, false, false);
+        }
+
+        public PingStatus Ping(uint curAddress, uint ttl, bool doLog)
+        {
+            return Ping(curAddress, ttl, doLog, false);
+        }
+
+        public PingStatus Ping(uint curAddress, uint ttl, bool doLog, bool useUdp)
+        {
+            Ping p = new Ping();
+            PingOptions po = new PingOptions((int)ttl, true);
+            
+            int timeout = 3000;
+            byte[] buffer = Encoding.Default.GetBytes("abcd");
+
+            PingReply pr =
+                p.Send(new IPAddress((long)curAddress), timeout, buffer, po);
+
+            PingStatus ps = new PingStatus();
+
+            ps.success = pr.Status == IPStatus.Success;
+            ps.error = pr.Status;
+            ps.status = pr.Status;
+            ps.ttl = ttl;
+            ps.destinationAddress = curAddress;
+            ps.delay = pr.RoundtripTime;
+
+            return ps;
+        }
+
+        public void PIcmpErr(IPStatus nIcmpErr)
+        {
         }
     }
 }
